3031ca6016966a87a0735dfcc617378dfde8431e
jxxnkyeong12 <znzlzn651@gmail.com>
2022-07-12 17:48:31 +0900

    Android01_안녕

---
+++ Android01_HelloWorld/app/src/main/java/com/example/andi01_helloworld/StudentMain.java
@@ -0,0 +1,18 @@
+package com.example.andi01_helloworld;
+
+public class StudentMain {
+    //주석입니다
+    //변수선언
+     int num1, num2;
+
+    //num1에 10담아보기
+        public static void main(String[] args) {
+
+            StudentMain sm = new StudentMain();
+            sm.num1 =10;
+            //syso <-이건 자바 ...ㅎ 자동완성 안돼 ㅠ
+            System.out.println(sm.num1);
+        }
+
+
+}
